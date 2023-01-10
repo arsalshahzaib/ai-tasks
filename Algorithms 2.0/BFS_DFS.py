cc621757ab426ccf9e2aa73b71a2c4bf9820e028
Arsal Shahzaib Bin Mushtaq <sp20-bcs-109@cuilahore.edu.pk>
2023-01-10 21:56:27 +0500

    Update BFS_DFS.py

--- Algorithms 2.0/BFS_DFS.py
+++ Algorithms 2.0/BFS_DFS.py
@@ -10,10 +10,11 @@ MyGraph={
 def DFS_Iterative_Algorithm(MyGraph,Start):
     Stack,Path=[Start],[]
     while Stack:
-        Vertex=Stack.pop()
+        Vertex = Stack.pop() # if pop argument is null, then it's DFS. and if it is '0', then it is BFS
         if Vertex not in Path:
             Path.append(Vertex)
             for N in MyGraph[Vertex]:
                 Stack.append(N)
     return Path
+
 print(DFS_Iterative_Algorithm(MyGraph,'A'))
